3e6c32d8ac79276eda6f71fb7161eb2fce834d7d
Ibrahim <79383289+ibrakhimus@users.noreply.github.com>
2023-02-03 11:30:59 -0500

    api

--- index.js
+++ index.js
@@ -2,9 +2,15 @@
 
 
 async function main() {
-const movies = await fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=68fafaba&s=${movieName}`);
+const movies = await fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=68fafaba&s=${title}`);
 const moviesPosts = await movies.json();
-console.log(moviesPosts)
 }
 
 main(); 
+
+async function onSearchChange(event) {
+    const id = event.target.value;
+    main(id);
+}
+
+console.log(localStorage)
---
+++ movie.js
@@ -0,0 +1,25 @@
+const movieListEl = document.querySelector('.movie__list');
+
+async function renderMovies() {
+    const movies = await fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=68fafaba&s=encanto`);
+    const moviesData = await movies.json();
+    console.log(moviesData)
+
+     movieListEl.innerHTML = moviesData.Search.map(movie => movieHTML(movie) ).join('');
+}
+
+renderMovies();
+
+function movieHTML(movie) {
+    return `<div class="movie">
+    <figure class="movie__img--wrapper">
+        <img class="movie__img" src='${movie.Poster}' alt="">
+    </figure>
+    <div class="movie__description">
+        <p class="movie__title">${movie.Title}</p>
+        <p class="movie__para">Type: ${movie.Type} </p>
+        <p class="movie__para">${movie.Year}</p>
+        <p class="movie__para">imdbID: ${movie.imdbID} </p>
+    </div>
+</div>`
+};
